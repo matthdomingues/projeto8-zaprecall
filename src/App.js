05da1699f9847f6e2a5d823d1aee6ddbc1acfb8f
root <root@DESKTOP-QJ6ICDP.localdomain>
2022-07-20 00:53:15 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import Navbar from "./Navbar";
+//import Corpo from "./Corpo";
+//import Mobile from "./Mobile";
+
+// lembrete: import React from 'react'; quando utilizar useState
+
+/*export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+            <Mobile />
+        </div>
+    );
+}*/
